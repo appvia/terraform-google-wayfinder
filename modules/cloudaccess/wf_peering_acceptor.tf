8b0066b7fe63c725a5a7fe9546d3217a503940de
Adam Smith <alice@Adams-MacBook-Pro.local>
2023-11-06 13:29:03 +0000

    make the role the role

--- modules/cloudaccess/wf_peering_acceptor.tf
+++ modules/cloudaccess/wf_peering_acceptor.tf
@@ -1,30 +1,30 @@
-resource "google_service_account" "networkmanager" {
-  count = var.enable_network_manager ? 1 : 0
+resource "google_service_account" "peeringacceptor" {
+  count = var.enable_peering_acceptor ? 1 : 0
 
   account_id   = "${local.resource_prefix}networkmgr${local.resource_suffix}"
   display_name = "Network Manager"
 }
 
-resource "google_project_iam_member" "networkmanager" {
-  count = var.enable_network_manager ? 1 : 0
+resource "google_project_iam_member" "peeringacceptor" {
+  count = var.enable_peering_acceptor ? 1 : 0
 
   project = data.google_project.project.id
   role    = "roles/owner"
-  member  = google_service_account.networkmanager[0].member
+  member  = google_service_account.peeringacceptor[0].member
 }
 
-resource "google_service_account_iam_member" "networkmanager" {
-  count = var.enable_network_manager && (var.from_gcp) ? 1 : 0
+resource "google_service_account_iam_member" "peeringacceptor" {
+  count = var.enable_peering_acceptor && (var.from_gcp) ? 1 : 0
 
-  service_account_id = google_service_account.networkmanager[0].name
+  service_account_id = google_service_account.peeringacceptor[0].name
   role               = "roles/iam.serviceAccountTokenCreator"
   member             = data.google_service_account.wayfinder[0].member
 }
 
-resource "google_service_account_iam_member" "networkmanagerfederated" {
-  count = var.enable_network_manager && (var.from_aws || var.from_azure) ? 1 : 0
+resource "google_service_account_iam_member" "peeringacceptorfederated" {
+  count = var.enable_peering_acceptor && (var.from_aws || var.from_azure) ? 1 : 0
 
-  service_account_id = google_service_account.networkmanager[0].name
+  service_account_id = google_service_account.peeringacceptor[0].name
   role               = "roles/iam.serviceAccountTokenCreator"
 
   # we should possibly make this more specific (although our pool already limits to the correct IRSA role)
