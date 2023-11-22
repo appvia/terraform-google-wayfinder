884bb34e69ae490c17006d31cf796982de36038c
Adam Smith <21332117+asmith030@users.noreply.github.com>
2023-11-22 16:48:17 +0000

    Refine the permissions used for gcp roles (#13)
    * Update permissions for roles
    
    * Change them perms yolo
    
    * Add a perm
    
    * dnszoneupdate
    
    * change this guy
    
    * Format again
    
    ---------
    
    Co-authored-by: Adam Smith <alice@Adams-MacBook-Pro.local>

--- modules/cloudaccess/wf_cluster_manager.tf
+++ modules/cloudaccess/wf_cluster_manager.tf
@@ -9,9 +9,52 @@ resource "google_project_iam_member" "clustermanager" {
   count = var.enable_cluster_manager ? 1 : 0
 
   project = data.google_project.project.id
-  role    = "roles/owner"
+  role    = google_project_iam_custom_role.clustermanager[0].name
   member  = google_service_account.clustermanager[0].member
 }
+
+resource "google_project_iam_custom_role" "clustermanager" {
+  count = var.enable_cluster_manager ? 1 : 0
+
+  role_id     = "${local.resource_prefix}clustermgr${local.resource_suffix}"
+  title       = "Cluster Manager"
+  description = "Permissions for wayfinder to manage Clusters"
+
+  permissions = [
+    "compute.firewalls.list",
+    "container.clusterRoleBindings.create",
+    "container.clusterRoleBindings.get",
+    "container.clusterRoles.bind",
+    "container.clusterRoles.create",
+    "container.clusters.create",
+    "container.clusters.delete",
+    "container.clusters.get",
+    "container.clusters.getCredentials",
+    "container.clusters.list",
+    "container.clusters.update",
+    "container.operations.get",
+    "container.operations.list",
+    "container.podSecurityPolicies.create",
+    "container.secrets.create",
+    "container.secrets.get",
+    "container.serviceAccounts.create",
+    "container.serviceAccounts.createToken",
+    "container.serviceAccounts.get",
+    "container.services.delete",
+    "container.services.list",
+    "iam.serviceAccounts.actAs",
+    "iam.serviceAccounts.create",
+    "iam.serviceAccounts.delete",
+    "iam.serviceAccounts.get",
+    "iam.serviceAccounts.getIamPolicy",
+    "iam.serviceAccounts.list",
+    "iam.serviceAccounts.setIamPolicy",
+    "resourcemanager.projects.get",
+    "resourcemanager.projects.getIamPolicy",
+    "resourcemanager.projects.setIamPolicy",
+  ]
+}
+
 
 resource "google_service_account_iam_member" "clustermanager" {
   count = var.enable_cluster_manager && (var.from_gcp) ? 1 : 0
