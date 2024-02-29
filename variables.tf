aefa6b8d841810256f3cc64bfdf7a0fa1baf8f14
Kashif Saadat <kashifsaadat@gmail.com>
2024-02-29 16:26:51 +0000

    Wayfinder v2.6.4 and GKE v1.28 (#17) (#18)

--- variables.tf
+++ variables.tf
@@ -104,7 +104,7 @@ variable "gke_release_channel" {
 variable "gke_version" {
   description = "The version to use for GKE."
   type        = string
-  default     = "1.27"
+  default     = "1.28"
 }
 
 variable "labels" {
@@ -189,5 +189,5 @@ variable "wayfinder_release_channel" {
 variable "wayfinder_version" {
   description = "The version to use for Wayfinder."
   type        = string
-  default     = "v2.5.1"
+  default     = "v2.6.4"
 }
