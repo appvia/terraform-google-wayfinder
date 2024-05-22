27fa6a2ee42fad8bf23983cf044d62de175ebe9d
Kashif Saadat <kashifsaadat@gmail.com>
2024-05-22 12:54:34 +0100

    CloudAccess Module: Add missing ClusterManager permissions and enable Google Service APIs (#19)
    * fix: add missing permissions needed for ClusterManager
    
    * fix: correct the variable references in the cloudaccess module example
    
    * fix: enable Google APIs on Project if required
    
    * fix: enable Google Compute Engine API for ClusterManager role
    
    * fix: enable Google Container API for ClusterManager role

--- modules/cloudaccess/examples/complete/main.tf
+++ modules/cloudaccess/examples/complete/main.tf
@@ -5,8 +5,8 @@ module "wayfinder_cloudaccess" {
 
   wayfinder_identity_gcp_service_account = var.wayfinder_identity_gcp_service_account
   wayfinder_identity_aws_role_arn        = var.wayfinder_identity_aws_role_arn
-  wayfinder_identity_azure_client_id     = var.wayfinder_identity_client_principal_id
-  wayfinder_identity_azure_tenant_id     = var.wayfinder_identity_client_tenant_id
+  wayfinder_identity_azure_client_id     = var.wayfinder_identity_azure_client_id
+  wayfinder_identity_azure_tenant_id     = var.wayfinder_identity_azure_tenant_id
 
   enable_cluster_manager  = var.enable_cluster_manager
   enable_dns_zone_manager = var.enable_dns_zone_manager
--- modules/cloudaccess/main.tf
+++ modules/cloudaccess/main.tf
@@ -14,3 +14,25 @@ data "google_service_account" "wayfinder" {
 
   account_id = var.wayfinder_identity_gcp_service_account
 }
+
+resource "google_project_service" "compute_api" {
+  count   = var.enable_cluster_manager ? 1 : 0
+  project = data.google_project.project.project_id
+  service = "compute.googleapis.com"
+}
+
+resource "google_project_service" "compute_api" {
+  count   = var.enable_cluster_manager ? 1 : 0
+  project = data.google_project.project.project_id
+  service = "container.googleapis.com"
+}
+
+resource "google_project_service" "cloud_resource_manager_api" {
+  project = data.google_project.project.project_id
+  service = "cloudresourcemanager.googleapis.com"
+}
+
+resource "google_project_service" "iam_credentials_api" {
+  project = data.google_project.project.project_id
+  service = "iamcredentials.googleapis.com"
+}
